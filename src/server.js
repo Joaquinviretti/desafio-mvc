e4bb459dd9adf788d094c7f374b804d6a97d2622
Joaquin Viretti <joaquin.viretti@gmail.com>
2022-07-01 10:10:17 -0300

    moving views folder outside public

--- src/server.js
+++ src/server.js
@@ -25,15 +25,15 @@ app.engine(
     'hbs', engine({
         extname:"hbs",
         defaultLayout:"main.hbs",
-        layoutsDir: path.join(__dirname, "..", "./public/views/layouts"),
-        partialsDir: path.join(__dirname, "..", "./public/views/partials")
+        layoutsDir: path.join(__dirname, "..", "./views/layouts"),
+        partialsDir: path.join(__dirname, "..", "./views/partials")
     }) 
 )
 
 app.use(express.static('../public'))
 
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, '..', 'public/views'))
+app.set("views", path.join(__dirname, '..', 'views'))
 
 //MIDDLEWARE para validad ID
 let validarId = (req, res, next) => {
